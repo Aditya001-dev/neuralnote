3e6a04a332b07760db4d4ed25e454e72a49103b8
Aditya <adityanair1126@gmail.com.>
2026-05-11 11:45:09 +0530

    fix: remove clerk for deployment

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,2 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
-
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect();
-  }
-});
-
-export const config = {
-  matcher: ['/((?!_next|_not-found|favicon.ico).*)', '/(api|trpc)(.*)'],
-};
+export default function middleware() {}
+export const config = { matcher: [] };
